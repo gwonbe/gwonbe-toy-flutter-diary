ae2d6727139d836c67d6f3ef5e5cb73823117e9d
gwonbe <syj021231@naver.com>
2025-03-19 19:21:49 +0900

    2025.03.19.수

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:diary/home/home.dart';
 import 'package:diary/provider/date_time_provider.dart';
 import 'package:diary/section/settings.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
@@ -33,6 +36,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    FlutterNativeSplash.remove();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
